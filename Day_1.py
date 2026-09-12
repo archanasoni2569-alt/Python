2d0f550e86d203178013b60da5630a3d9bba1e1d
archanasoni2569 <archanasoni2569@gmail.com>
2026-09-13 00:18:30 +0530

    aaded

--- Day_1.py
+++ Day_1.py
@@ -38,6 +38,56 @@
 # => it is platform dependent.
 # => EXAMPLE: Appstore, playstore,apple music etc.
 # (c) Hybrid application :-
-# => it is a combination of Native and mobile web applications.
+# => it is a combination of Native and mobile web appl
 # => it runs as a website as well as runs on android and iOS.
 # => EXAMPLE: Youtube, WhatsApp, Facebook, Instagram etc.
+y: 2
+
+3.Desktop Application:-
+=> it runs only on desktop.
+=> it does not require any internet connection.
+=>EXAMPLE: Excel, Power-point, Ms-word, NOtepad.
+
+https://docs.google.com/forms/d/e/1FAIpQLScSydsXIC3I0iUtjdR6LM0Nd-ikGD9r0LdG_i6D7zMOTKgaWA/viewform?usp=dialog
+
+https://docs.google.com/forms/d/e/1FAIpQLSfmb6GeEfqaDXzz64GzSQ7I38Ra1DfDttJe6rcIckj7EqtOYw/viewform?usp=header
+
+TASK: Create a registration form just like google form
+2. Explore Forntend, Backend, and Database. 
+
+
+3.Desktop Application:-
+=> it runs on only desktop.
+=> it does not require any internet connection.
+=>EXAMPLE: Excel, Power-point, Ms-word, NOtepad.
+Day: 3
+
+
+## Website:-
+=> Parts:___
+1. FRONTEND => User visible 
+2. Backend => Owner accessible and accessible by Person.
+
+1.Frontend => Parts of web development that deal with User Interface(UI).
+
+## TECHNOLOGY :-
+=> Html => it is used to build the body of any web page.
+=> CSS => it is used to apply styling on any web page.
+=> CSS(Library) => Bootstrap, Tailwind
+=> JavaScript 
+=> React/reat.js => it works componenet based 
+
+## why => First impression( Jo dikhega whi Bikega)
+
+
+
+2. Backend => the part of web development that deals with server side logic.
+
+## Technology:-
+=> Python(Django,Flask,Pandas,NumPy) , JavaScript(Node.js), Java(Springboot), Php(Laravel). 
+
+
+3. Database :-
+=> it is used to store our data in the structured form .
+=> it handles security issues.
+=>…
